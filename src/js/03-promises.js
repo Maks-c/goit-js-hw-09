66374a95623728bf2908a9a46c55fa2122f653a4
Maks <maksym.shershen@gmail.com>
2021-11-08 19:17:08 +0100

    add

--- src/js/03-promises.js
+++ src/js/03-promises.js
@@ -12,7 +12,6 @@ function onFormSubmit(element) {
   step = Number(step.value);
   delay = Number(delay.value);
 
-
   for (let position = 1; position <= amount; position += 1) {
     createPromise(position, delay)
       .then(({ position, delay }) => {
@@ -28,8 +27,6 @@ function onFormSubmit(element) {
     delay += step;
   }
 }
-
-
 function createPromise(position, delay) {
   const shouldResolve = Math.random() > 0.3;
   return new Promise((resolve, reject) => {
